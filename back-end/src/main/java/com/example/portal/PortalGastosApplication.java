9fb8db316c02d495185ed45ed8ca6352b619d7de
Jhony de Paula <jhony.paulaa@gmail.com>
2023-07-03 16:37:45 -0300

    add backend

---
+++ back-end/src/main/java/com/example/portal/PortalGastosApplication.java
@@ -0,0 +1,13 @@
+package com.example.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalGastosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalGastosApplication.class, args);
+	}
+
+}
